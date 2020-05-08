df7920f44ae28090698a4db00531fbd858bdc15a
Shu Uesugi <shu@chibicode.com>
2020-05-07 22:04:57 -0700

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,11 +13,12 @@ module.exports = {
       extends: [
         'eslint:recommended',
         'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
+        'prettier',
         'prettier/@typescript-eslint',
         'prettier/react',
-        'plugin:prettier/recommended',
       ],
       rules: {
         '@typescript-eslint/explicit-function-return-type': 'off',
