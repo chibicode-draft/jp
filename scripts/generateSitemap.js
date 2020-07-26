4f378f98a6d5a0a550b771c5159c56b97852c954
Shusaku Uesugi <shu@chibicode.com>
2020-07-25 21:24:35 -0700

    Fix otherPosts

--- scripts/generateSitemap.js
+++ scripts/generateSitemap.js
@@ -1,7 +1,7 @@
 const { createWriteStream } = require('fs')
 const { SitemapStream } = require('sitemap')
 const oldPosts = require('../src/contents/oldPosts.json')
-const regularPosts = require('../src/contents/regularPosts.json')
+const otherPosts = require('../src/contents/otherPosts.json')
 const featuredPosts = require('../src/contents/featuredPosts.json')
 const hiddenPosts = require('../src/contents/hiddenPosts.json')
 
@@ -11,7 +11,7 @@ const sitemap = new SitemapStream({ hostname: 'https://jp.chibicode.com' })
 const writeStream = createWriteStream('./public/sitemap.xml')
 sitemap.pipe(writeStream)
 sitemap.write('/')
-;[...oldPosts, ...featuredPosts, ...regularPosts, ...hiddenPosts]
+;[...oldPosts, ...featuredPosts, ...otherPosts, ...hiddenPosts]
   .filter(({ href }) => href.startsWith('/'))
   .forEach(({ href }) => {
     sitemap.write(href)
