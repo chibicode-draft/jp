b1420aac3b2de6435805ffcabc87f4b9ab8bd560
Shusaku Uesugi <shu@chibicode.com>
2020-08-01 15:18:08 -0700

    Upgrade packages

--- next.config.js
+++ next.config.js
@@ -2,24 +2,22 @@ const oldPosts = require('./src/contents/oldPosts.json')
 const hiddenPosts = require('./src/contents/hiddenPosts.json')
 
 module.exports = {
-  experimental: {
-    async rewrites() {
-      return [
-        {
-          source: '/jp-2019/:path*',
-          destination: 'https://jp-2019.chibicode.com/:path*',
-        },
-        {
-          source: '/static/:path*',
-          destination: 'https://jp-2019.chibicode.com/static/:path*',
-        },
-        ...[...oldPosts, ...hiddenPosts]
-          .filter(({ href }) => href.startsWith('/'))
-          .map(({ href }) => ({
-            source: `${href}{/}?`,
-            destination: `https://jp-2019.chibicode.com${href}`,
-          })),
-      ]
-    },
+  async rewrites() {
+    return [
+      {
+        source: '/jp-2019/:path*',
+        destination: 'https://jp-2019.chibicode.com/:path*',
+      },
+      {
+        source: '/static/:path*',
+        destination: 'https://jp-2019.chibicode.com/static/:path*',
+      },
+      ...[...oldPosts, ...hiddenPosts]
+        .filter(({ href }) => href.startsWith('/'))
+        .map(({ href }) => ({
+          source: `${href}{/}?`,
+          destination: `https://jp-2019.chibicode.com${href}`,
+        })),
+    ]
   },
 }
